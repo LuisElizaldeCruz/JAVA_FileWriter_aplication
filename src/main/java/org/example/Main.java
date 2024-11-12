788d64528a93ae621368f980ca521dcda6191161
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-11-11 22:16:19 -0600

    creación del menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Insertar datos");
+            System.out.println("2. Leer datos");
+            System.out.println("3. Modificar datos");
+            System.out.println("4. Eliminar datos");
+            System.out.println("5. Salir");
+            System.out.print("Selecciona una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    pedirDatos();
+                    break;
+                case 2:
+                    // leerDatos();
+                    break;
+                case 3:
+                    // modificarDatos();
+                    break;
+                case 4:
+                    // eliminarDatos();
+                    break;
+                case 5:
+                    System.out.println("Gracias por usar el programa");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+        } while (opcion != 5);
+    }
+
+    public static void pedirDatos() {
+        Scanner scanner = new Scanner(System.in);
 
         String genre = null;
         generatedFile gf = new generatedFile();
@@ -28,11 +63,11 @@ public class Main {
         int option = scanner.nextInt();
         scanner.nextLine(); // Limpia el buffer de entrada
 
-        if(option == 1){
+        if (option == 1) {
             genre = "M";
-        }else if(option == 2){
+        } else if (option == 2) {
             genre = "F";
-        } else{
+        } else {
             System.out.println("variable incorrecta");
             return;
         }
@@ -40,6 +75,6 @@ public class Main {
         System.out.print("Introduce tu clave de licenciatura: ");
         String claveLicenciatura = scanner.nextLine();
 
-        gf.generarFormato( new Student(matricula, nombre, primerApellido, segundoApellido, claveLicenciatura, genre));
+        gf.generarFormato(new Student(matricula, nombre, primerApellido, segundoApellido, claveLicenciatura, genre));
     }
 }
